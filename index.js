0ca003f548a7e912e3c972da48763c4ad85741a1
Pranta07 <palpranta@gmail.com>
2022-01-31 02:03:57 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxq3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log("database connected");
+    // client.close();
+});
 
 app.get("/", (req, res) => {
     res.send("lady running!");
