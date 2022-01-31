552f72cc06128add873e88e1025dba3475025072
Pranta07 <palpranta@gmail.com>
2022-02-01 02:22:10 +0600

    get api for trending collection

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxq3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//middleware
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxq3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,7 +19,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        console.log("database connected");
+        // console.log("database connected");
+        const database = client.db("LadyShop");
+        const trendingCollection = database.collection("Trending");
+
+        app.get("/trending", async (req, res) => {
+            const result = await trendingCollection.find({}).toArray();
+            res.json(result);
+        });
     } finally {
         // await client.close();
     }
